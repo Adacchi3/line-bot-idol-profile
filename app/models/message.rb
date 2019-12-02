62a818bbc8eca06f9c9b4c99735f66e3baf893a7
Adacchi3 <adachi.prog@gmail.com>
2019-12-02 21:29:14 +0900

    [fix] create message method

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,29 +1,28 @@
 class Message < ApplicationRecord
   def self.send_message(client, events)
     events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: "flex",
-            altText: "アイドルのプロフィールを閲覧しました。",
-            contents: Idol.profile
-          }
-          client.reply_message(event['replyToken'], message)
-        else
-          message = {
-            type: 'text',
-            text: 'メモ帳はその形式での検索に対応しておりません。'
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-      end
+      message = create_message(event)
+      client.reply_message(event['replyToken'], message)
     }
   end
 
   private
   def self.create_message(event)
-  
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        message = {
+          type: "flex",
+          altText: "アイドルのプロフィールを閲覧しました。",
+          contents: Idol.profile
+        }
+      else
+        message = {
+          type: 'text',
+          text: 'メモ帳はその形式での検索に対応しておりません。'
+        }
+      end
+    end
   end
 end
